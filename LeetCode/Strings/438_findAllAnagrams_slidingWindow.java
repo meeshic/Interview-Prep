b0c918f9254aed8a81029fd8a7bb9df436632272
meeshic <michellecsr@gmail.com>
2018-03-31 19:13:37 -0700

    438. Implemented with sliding window technique

---
+++ LeetCode/Strings/438_findAllAnagrams_slidingWindow.java
@@ -0,0 +1,68 @@
+//LeetCode 438. Find All Anagrams in a String
+
+/*
+Question:
+Given a string s and a non-empty string p, find all the start indices of p's anagrams in s.
+
+Strings consists of lowercase English letters only and the length of both strings s and p will not be larger than 20,100.
+
+The order of output does not matter.
+*/
+
+class Solution {
+    public List<Integer> findAnagrams(String s, String p) {
+        List<Integer> output = new ArrayList<Integer>();
+        if(s == null || s.length() == 0 || p == null || p.length() == 0) return output;
+        
+        int begin=0;
+        int end=0;
+        //anagram indicator
+        int count = p.length();
+        
+        int[] freq = new int[128];
+        
+        for(char c : p.toCharArray())
+            freq[c-97]++;
+        
+        while(end < s.length()){
+            char current = s.charAt(end);
+            //match of p within s
+            if(freq[current-97] > 0){
+                count--;
+            }
+            
+            freq[current-97]--;
+            end++;
+            
+            if(count == 0) output.add(begin);
+            
+            //window size has exceeded p.length()
+            if(end-begin == p.length()){
+                int b = s.charAt(begin++)-97;
+                if(freq[b] >= 0)
+                    count++;
+                freq[b]++;
+            }
+        }
+        return output;
+    }
+}
+
+
+/*
+Clarifying question(s):
+    - can null and/or empty strings be arguments?
+Algorithm:
+    - Naive brute force: O(M*N), M=s.length(), N=p.length(), and using hashmap to see if anagram
+        > iterate through string s and use each character as the beginning of a potential anagram
+        > check i...i+p.length()-1 to see if anagram
+        > if yes, add to output
+        > i++
+    - Sliding window technique: O(N), N=s.length(), using hashmap to keep track of matched characters
+        > use two 'pointers', begin and end that keep track of current potential anagram of p in s
+        > if window size is p.length(), we found anagram of p
+    - Optimized sliding window technique: O(N), using array to keep track of what's been seen in s
+        > use count variable to check if an anagram has been found
+        > the window size at end at of each iteration is <= p.length()
+        > count is only changed when it applies to the current potential anagram
+*/
